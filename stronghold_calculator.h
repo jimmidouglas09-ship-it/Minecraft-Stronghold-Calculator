6b5a67086cb0ea2a4d7b3867c52d19bd6f194288
Ma_ehy <jimmidouglas09@gmail.com>
2025-09-21 13:03:34 +0200

    Delete stronghold_calculator.h

--- stronghold_calculator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#define NOMINMAX
-#include "common.h"
-
-// Generate all possible stronghold cells
-void generateStrongholdCells();
-
-// Calculate stronghold locations based on player position and eye angle
-void calculateStrongholdLocationWithDistance(double playerX, double playerZ, double eyeAngle, double targetDistance = -1);
