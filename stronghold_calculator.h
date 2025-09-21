18830df66461b1f1396e2d7e628dd7894673ce75
Ma_ehy <jimmidouglas09@gmail.com>
2025-09-21 20:31:06 +0200

    Update stronghold_calculator.h

--- stronghold_calculator.h
+++ stronghold_calculator.h
@@ -1,9 +1,23 @@
+// stronghold_calculator.h - Simple header that uses common.h structures
 #pragma once
-#define NOMINMAX
+
+#ifndef STRONGHOLD_CALCULATOR_H
+#define STRONGHOLD_CALCULATOR_H
+
 #include "common.h"
 
-// Generate all possible stronghold cells
+// Core function declarations - structures are already defined in common.h
 void generateStrongholdCells();
+void calculateStrongholdLocationWithDistance(double playerX, double playerZ, double eyeAngle, double targetDistance = -1.0);
 
-// Calculate stronghold locations based on player position and eye angle
-void calculateStrongholdLocationWithDistance(double playerX, double playerZ, double eyeAngle, double targetDistance = -1);
+// Enhanced calculation function declarations
+double calculateMeasurementError(double alignError);
+double calculateCoordPrecisionError(double distance);
+double calculateFacingPrecisionError(double pixelError);
+double calculateCombinedError(double measurementError, double precisionError);
+std::pair<double, double> calculateEyeHoverPosition(double x1, double z1, double x2, double z2);
+
+// Utility function declarations
+double gaussianProbability(double x, double mean, double stdDev);
+
+#endif // STRONGHOLD_CALCULATOR_H
