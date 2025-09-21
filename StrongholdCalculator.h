33f8b3e9c770618ff413fdd9f0bf6b0ffa8ccb16
Ma_ehy <jimmidouglas09@gmail.com>
2025-09-21 16:47:51 +0200

    Update StrongholdCalculator.h

--- StrongholdCalculator.h
+++ StrongholdCalculator.h
@@ -1,6 +1,15 @@
 #pragma once
-
 #include "Common.h"
+
+// Structure to represent a chunk candidate within a stronghold cell
+struct ChunkCandidate {
+    int chunkX;           // Chunk X coordinate
+    int chunkZ;           // Chunk Z coordinate
+    int strongholdX;      // World X coordinate of stronghold (chunk*16 + 2)
+    int strongholdZ;      // World Z coordinate of stronghold (chunk*16 + 2)
+    double distanceFromRay; // Distance from the eye of ender ray
+    double probability;   // Probability based on distance from ray
+};
 
 class StrongholdCalculator {
 public:
@@ -15,4 +24,14 @@ private:
     static bool linesIntersect(double x1, double z1, double dx1, double dz1,
         double x2, double z2, double dx2, double dz2,
         double& intersectionX, double& intersectionZ);
+
+    // New methods for chunk-level precision
+    static ChunkCandidate findBestChunkInCell(const StrongholdCell& cell,
+        double rayX, double rayZ,
+        double directionX, double directionZ,
+        double standardDeviation);
+    static double calculateDistanceFromPointToRay(double pointX, double pointZ,
+        double rayX, double rayZ,
+        double directionX, double directionZ);
+    static double calculateGaussianProbability(double distance, double standardDeviation);
 };
